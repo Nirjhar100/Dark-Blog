e3886807b114fcebc4704eda94ffaddc2550e14b
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-04-30 10:53:46 +0530

    second-commit

--- CRUD_blog/settings.py
+++ CRUD_blog/settings.py
@@ -132,11 +132,13 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+#ALLOWED_HOSTS = ['041db62e.ngrok.io']
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
